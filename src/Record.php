fa18a8ab284d777f0d7abfe480881f0b0260affd
Alexander Letnikow <reclamme@gmail.com>
2018-11-21 11:06:13 +0200

    Add User Device record

---
+++ src/Record.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wearesho\Yii\UserDevice;
+
+use Carbon\Carbon;
+use yii\db;
+use yii\behaviors;
+
+/**
+ * Class Record
+ * @package Wearesho\Yii\UserDevice
+ *
+ * @property string $id [integer]
+ * @property string $user_id [integer]
+ * @property string $user_agent
+ * @property string $ip [varchar(39)]
+ * @property int $created_at [timestamp(0)]
+ * @property int $updated_at [timestamp(0)]
+ */
+class Record extends db\ActiveRecord
+{
+    public static function tableName(): string
+    {
+        return 'user_device';
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            'ts' => [
+                'class' => behaviors\TimestampBehavior::class,
+                'value' => function (): string {
+                    return Carbon::now()->toDateTimeString();
+                },
+            ],
+        ];
+    }
+
+    public function rules(): array
+    {
+        return [
+            [['user_id', 'user_agent', 'ip',], 'required',],
+            [['user_id',], 'integer', 'min' => 1,],
+            [['user_agnet',], 'string',],
+            [['ip',], 'ip',],
+            [['user_id', 'user_agent', 'ip',], 'unique',],
+        ];
+    }
+}
