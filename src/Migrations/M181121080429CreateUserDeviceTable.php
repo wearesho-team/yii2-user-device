33298dddcde940d24d295edea3be5f0346c9a0f5
Alexander Letnikow <reclamme@gmail.com>
2018-11-21 11:05:49 +0200

    Add migration for user_device table

---
+++ src/Migrations/M181121080429CreateUserDeviceTable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wearesho\Yii\UserDevice\Migrations;
+
+use yii\db\Migration;
+
+/**
+ * Class M181121080429CreateUserDeviceTable
+ */
+class M181121080429CreateUserDeviceTable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void
+    {
+        $this->createTable('user_device', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->unsigned()->notNull(),
+            'user_agent' => $this->text()->notNull(),
+            'ip' => $this->string(39)->notNull(),
+            'created_at' => $this->timestamp()->notNull()->defaultExpression('now()'),
+            'updated_at' => $this->timestamp()->notNull()->defaultExpression('now()'),
+        ]);
+
+        $this->createIndex('user_device_unique', 'user_device', [
+            'user_id',
+            'user_agent',
+            'ip',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): void
+    {
+        $this->dropTable('user_device');
+    }
+}
