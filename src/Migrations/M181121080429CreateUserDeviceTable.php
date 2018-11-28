f3452a5deaeb7f563390c5623f66d0f1e699c029
KartaviK <varkuta.roma@gmail.com>
2018-11-28 17:47:37 +0200

    Add length for user_agent for index

--- src/Migrations/M181121080429CreateUserDeviceTable.php
+++ src/Migrations/M181121080429CreateUserDeviceTable.php
@@ -25,7 +25,7 @@ class M181121080429CreateUserDeviceTable extends Migration
 
         $this->createIndex('user_device_unique', 'user_device', [
             'user_id',
-            'user_agent',
+            'user_agent(256)',
             'ip',
         ]);
     }
